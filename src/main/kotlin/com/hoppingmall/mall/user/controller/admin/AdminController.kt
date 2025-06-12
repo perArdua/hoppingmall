d335910930398e8a07401438117cfe13447c4f0f
Kim Tae Ho <kok8454@gmail.com>
2025-06-12 23:11:09 +0900

    chore: 불필요한 import문 제거

--- src/main/kotlin/com/hoppingmall/mall/user/controller/admin/AdminController.kt
+++ src/main/kotlin/com/hoppingmall/mall/user/controller/admin/AdminController.kt
@@ -4,12 +4,7 @@ import com.hoppingmall.mall.global.common.response.ApiResponse
 import com.hoppingmall.mall.user.dto.request.admin.SellerApprovalRequest
 import com.hoppingmall.mall.user.service.admin.AdminCommandService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/admin")
