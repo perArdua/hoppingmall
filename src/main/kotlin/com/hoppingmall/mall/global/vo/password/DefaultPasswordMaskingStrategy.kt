c553aa3341bad4693c5e07c776bdc42990c04520
Kim Tae Ho <kok8454@gmail.com>
2025-06-12 23:02:34 +0900

    refactor: 비밀번호 정책 책임 분리 및 도메인 예외 적용
    - PasswordPolicy 인터페이스 및 Default 구현 분리
    - PasswordCreator에 PasswordPolicy 주입하여 정책 위임
    - assertMatches 내부 require 제거 -> PasswordNotMatchedException 생성
    - PasswordErrorCode에 WEAK_PASSWORD, PASSWORD_NOT_MATCHED 추가
    - UserDomainService에서 validateNewUser 시 정책 검증까지 위임

--- src/main/kotlin/com/hoppingmall/mall/global/vo/password/DefaultPasswordMaskingStrategy.kt
+++
@@ -1,5 +0,0 @@
-package com.hoppingmall.mall.global.vo.password
-
-object DefaultPasswordMaskingStrategy : PasswordMaskingStrategy {
-    override fun mask(): String = "******"
-}
--- src/main/kotlin/com/hoppingmall/mall/global/vo/password/PasswordMaskingStrategy.kt
+++
@@ -1,5 +0,0 @@
-package com.hoppingmall.mall.global.vo.password
-
-fun interface PasswordMaskingStrategy {
-    fun mask(): String
-}
