d8e4a6e600d449c6ecfb8abac877c83917f59819
Kim Tae Ho <kok8454@gmail.com>
2025-06-14 03:28:06 +0900

    test: 로그인 기능 단위 테스트 및 컨트롤러 테스트 추가
    - 로그인 성공/실패 케이스 검증
    - Controller 단 JWT 반환 검증

--- src/test/kotlin/com/hoppingmall/mall/user/controller/user/UserControllerTest.kt
+++ src/test/kotlin/com/hoppingmall/mall/user/controller/user/UserControllerTest.kt
@@ -2,9 +2,12 @@ package com.hoppingmall.mall.user.controller.user
 
 import com.hoppingmall.mall.global.common.response.ApiResponse
 import com.hoppingmall.mall.global.enums.Role
+import com.hoppingmall.mall.user.dto.request.user.SignInRequest
 import com.hoppingmall.mall.user.dto.request.user.SignUpRequest
+import com.hoppingmall.mall.user.dto.response.user.SignInResponse
 import com.hoppingmall.mall.user.dto.response.user.SignUpResponse
 import com.hoppingmall.mall.user.service.user.UserCommandService
+import com.hoppingmall.mall.user.service.user.UserQueryService
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -13,9 +16,11 @@ import org.mockito.kotlin.whenever
 class UserControllerTest {
 
     private val userCommandService: UserCommandService = mock()
+    private val userQueryService: UserQueryService = mock()
 
     private val controller = UserController(
         userCommandService = userCommandService,
+        userQueryService = userQueryService
     )
 
     @Test
@@ -39,6 +44,29 @@ class UserControllerTest {
 
         // when
         val response: ApiResponse<SignUpResponse> = controller.signUp(request)
+
+        // then
+        assertEquals("SUCCESS", response.code)
+        assertEquals("성공", response.message)
+        assertEquals(expectedResponse, response.data)
+    }
+
+    @Test
+    fun `로그인 요청이 성공하면 토큰 정보가 포함된 응답이 반환된다`() {
+        // given
+        val request = SignInRequest(
+            email = "test@example.com",
+            password = "password123"
+        )
+
+        val expectedResponse = SignInResponse(
+            accessToken = "access-token"
+        )
+
+        whenever(userQueryService.login(request)).thenReturn(expectedResponse)
+
+        // when
+        val response: ApiResponse<SignInResponse> = controller.login(request)
 
         // then
         assertEquals("SUCCESS", response.code)
