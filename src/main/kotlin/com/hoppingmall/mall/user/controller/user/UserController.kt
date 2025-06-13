a2bdaa4858c68c7b7966ba9b9a3f44e263db8aac
Kim Tae Ho <kok8454@gmail.com>
2025-06-14 03:27:07 +0900

    feat: 로그인 API 컨트롤러 구현
    - POST /api/v1/users/login 엔드포인트 추가

--- src/main/kotlin/com/hoppingmall/mall/user/controller/user/UserController.kt
+++ src/main/kotlin/com/hoppingmall/mall/user/controller/user/UserController.kt
@@ -1,9 +1,12 @@
 package com.hoppingmall.mall.user.controller.user
 
 import com.hoppingmall.mall.global.common.response.ApiResponse
+import com.hoppingmall.mall.user.dto.request.user.SignInRequest
 import com.hoppingmall.mall.user.dto.request.user.SignUpRequest
+import com.hoppingmall.mall.user.dto.response.user.SignInResponse
 import com.hoppingmall.mall.user.dto.response.user.SignUpResponse
 import com.hoppingmall.mall.user.service.user.UserCommandService
+import com.hoppingmall.mall.user.service.user.UserQueryService
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userCommandService: UserCommandService,
+    private val userQueryService: UserQueryService
 ) {
 
     @PostMapping("/signup")
@@ -21,6 +25,14 @@ class UserController(
         @RequestBody @Valid request: SignUpRequest
     ): ApiResponse<SignUpResponse> {
         val response = userCommandService.signUp(request)
+        return ApiResponse.Companion.success(response)
+    }
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody @Valid request: SignInRequest
+    ): ApiResponse<SignInResponse> {
+        val response = userQueryService.login(request)
         return ApiResponse.Companion.success(response)
     }
 }
