2396fc98419783abd681a434e2a85f9eb5f44ff9
Kim Tae Ho <kok8454@gmail.com>
2025-06-18 20:30:37 +0900

    refactor: 로그인 책임을 UserQueryService -> AuthService로 위임

--- src/main/kotlin/com/hoppingmall/mall/user/service/user/UserQueryServiceImpl.kt
+++ src/main/kotlin/com/hoppingmall/mall/user/service/user/UserQueryServiceImpl.kt
@@ -1,11 +1,10 @@
 package com.hoppingmall.mall.user.service.user
 
-import com.hoppingmall.mall.global.jwt.TokenProvider
 import com.hoppingmall.mall.global.vo.email.Email
 import com.hoppingmall.mall.global.vo.password.service.PasswordVerifier
+import com.hoppingmall.mall.user.domain.User
 import com.hoppingmall.mall.user.domain.repository.UserRepository
 import com.hoppingmall.mall.user.dto.request.user.SignInRequest
-import com.hoppingmall.mall.user.dto.response.user.SignInResponse
 import com.hoppingmall.mall.user.exception.user.UserLoginFailedException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,19 +13,16 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class UserQueryServiceImpl(
     private val userRepository: UserRepository,
-    private val tokenProvider: TokenProvider,
-    private val passwordVerifier: PasswordVerifier
+    private val passwordVerifier: PasswordVerifier,
 ) : UserQueryService {
 
-    override fun login(request: SignInRequest): SignInResponse {
+    override fun authenticate(request: SignInRequest): User {
         val email = Email(request.email)
         val user = userRepository.findByEmail(email)
             ?: throw UserLoginFailedException()
 
         passwordVerifier.assertMatches(request.password, user.getPassword())
 
-        val token = tokenProvider.generateToken(user.id!!, user.getRole())
-
-        return SignInResponse(accessToken = token)
+        return user
     }
 }
