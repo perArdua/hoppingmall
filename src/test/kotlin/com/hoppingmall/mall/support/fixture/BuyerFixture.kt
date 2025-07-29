f49bc11d71ec67215b8088c7be532c7ff0d2f0a2
perardua <coglyi@naver.com>
2025-07-30 07:35:19 +0900

    test: test 관련 코드 수정 - assert 수정 - Buyer, Seller Fixture에서 withId 사용하도록 함

--- src/test/kotlin/com/hoppingmall/mall/support/fixture/BuyerFixture.kt
+++ src/test/kotlin/com/hoppingmall/mall/support/fixture/BuyerFixture.kt
@@ -3,15 +3,16 @@ package com.hoppingmall.mall.support.fixture
 import com.hoppingmall.mall.global.enums.Role
 import com.hoppingmall.mall.global.vo.email.Email
 import com.hoppingmall.mall.global.vo.password.Password
+import com.hoppingmall.mall.support.withId
 import com.hoppingmall.mall.user.domain.Buyer
 import com.hoppingmall.mall.user.domain.User
 
 fun Buyer.Companion.fixture(): Buyer {
-    val user = User.fixture(
+    val user = User.create(
         email = Email("buyer@example.com"),
         password = Password("encoded"),
         name = "구매자",
         role = Role.BUYER
-    )
+    ).withId(1L)
     return Buyer.create(user)
 }
