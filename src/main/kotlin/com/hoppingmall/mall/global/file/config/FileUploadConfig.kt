739ad919e13cb7e5c4a0cc2f4021892f76c58aa3
perardua <coglyi@naver.com>
2026-03-12 21:27:04 +0900

    feat(global): FileUploadConfig OS 독립 경로 전환 - baseUploadDir 기본값을 D:/hoppingmall에서 ${user.home}/hoppingmall로 변경 - defaultImagePath 계산 프로퍼티 추가

--- src/main/kotlin/com/hoppingmall/mall/global/file/config/FileUploadConfig.kt
+++ src/main/kotlin/com/hoppingmall/mall/global/file/config/FileUploadConfig.kt
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component
 @Component
 @ConfigurationProperties(prefix = "file.upload")
 class FileUploadConfig {
-    
-    var baseUploadDir: String = "D:/hoppingmall"
+
+    var baseUploadDir: String = "${System.getProperty("user.home")}/hoppingmall"
     var maxFileSize: Long = 5 * 1024 * 1024
     var allowedExtensions: Set<String> = setOf("jpg", "jpeg", "png", "gif", "webp")
     var allowedDomains: Set<String> = setOf("product")
     var subDirectory: String = "images"
-} 
+
+    val defaultImagePath: String
+        get() = "$baseUploadDir/product/images/default-product.jpg"
+}
