edf6e0dd4d64ee8a763ddf657f38a10f12475d6c
perardua <coglyi@naver.com>
2026-03-21 19:23:02 +0900

    fix(global): Docker 빌드에서 hoppingmall-common 모듈 참조 수정 - 각 서비스 Dockerfile에 COPY hoppingmall-common 추가 - settings.gradle.kts: includeBuild 경로를 조건부 탐색 (로컬: ../, Docker: /) - CI: Docker 빌드 전 공통 모듈을 서비스 디렉토리에 복사

--- user-service/settings.gradle.kts
+++ user-service/settings.gradle.kts
@@ -1,3 +1,7 @@
 rootProject.name = "user-service"
 
-includeBuild("../hoppingmall-common")
+val commonPath = if (file("../hoppingmall-common").exists()) "../hoppingmall-common"
+    else if (file("/hoppingmall-common").exists()) "/hoppingmall-common"
+    else null
+
+commonPath?.let { includeBuild(it) }
